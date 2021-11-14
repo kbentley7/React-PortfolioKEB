77ca1c4ec3dae748d397c6f8017beab724e62f41
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-11-13 21:48:39 -0500

    edited package.json

--- public/src/components/About.js
+++ public/src/components/About.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import me from '../images/new-me.png'
+import me from ''
 
 export default function About() {
   return (
@@ -11,7 +11,7 @@ export default function About() {
       </div>
       <br></br> <br></br>
       <p id="about-me-text" className="central-content">
-      I am an aspiring web developer, currently studying at Penn LPS's Bootcamp. My background is in education. I have my M.S.Ed from UPenn, with a specialization in second language acquisition. So, as you might have guessed, I'm a huge language nerd - I'm fluent in Spanish, Portuguese, Japanese, HTML, CSS, and Javascript. Oh, and English too, I guess.
+      I am 
       </p>
     </div>
   );
